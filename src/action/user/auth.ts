60471af8b8217a01d10e16aa4b6885aad7dc7d3b
MaxtuneLee <max@xox.im>
2024-09-13 13:49:42 +0800

    feat: test login

--- src/action/user/auth.ts
+++ src/action/user/auth.ts
@@ -6,59 +6,75 @@ import { createSession, deleteSession } from "@/lib/session";
 import { redirect } from "next/navigation";
 
 export async function loginFromX(
-	openid: string,
-	userIdentifier: string,
-	type: "feishu" | "link"
+  openid: string,
+  userIdentifier: string,
+  type: "feishu" | "link"
 ) {
-	console.log("loginFrom", type, openid, userIdentifier);
-	let uidList: { uid: number }[] | null = null;
-	// check if openid exists
-	if (type === "feishu") {
-		uidList = await db
-			.select({
-				uid: user.id,
-			})
-			.from(user)
-			.where(eq(user.feishuOpenId, openid));
-		if (!uidList || uidList.length === 0) {
-			uidList = await db
-				.insert(user)
-				.values({
-					feishuOpenId: openid,
-					name: userIdentifier,
-					createdAt: new Date(),
-					updatedAt: new Date(),
-				})
-				.returning({ uid: user.id });
-		}
-	} else if (type === "link") {
-		uidList = await db
-			.select({
-				uid: user.id,
-			})
-			.from(user)
-			.where(eq(user.sastLinkOpenId, openid));
-		if (!uidList || uidList.length === 0) {
-			uidList = await db
-				.insert(user)
-				.values({
-					sastLinkOpenId: openid,
-					name: userIdentifier,
-					studentId: userIdentifier,
-					createdAt: new Date(),
-					updatedAt: new Date(),
-				})
-				.returning({ uid: user.id });
-		}
-	}
-	if (uidList && uidList.length > 0) {
-		await createSession(uidList[0].uid, userIdentifier, type);
-	} else {
-		throw new Error("login failed");
-	}
+  console.log("loginFrom", type, openid, userIdentifier);
+  let uidList: { uid: number }[] | null = null;
+  // check if openid exists
+  if (type === "feishu") {
+    uidList = await db
+      .select({
+        uid: user.id,
+      })
+      .from(user)
+      .where(eq(user.feishuOpenId, openid));
+    if (!uidList || uidList.length === 0) {
+      uidList = await db
+        .insert(user)
+        .values({
+          feishuOpenId: openid,
+          name: userIdentifier,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        })
+        .returning({ uid: user.id });
+    }
+  } else if (type === "link") {
+    uidList = await db
+      .select({
+        uid: user.id,
+      })
+      .from(user)
+      .where(eq(user.sastLinkOpenId, openid));
+    if (!uidList || uidList.length === 0) {
+      uidList = await db
+        .insert(user)
+        .values({
+          sastLinkOpenId: openid,
+          name: userIdentifier,
+          studentId: userIdentifier,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        })
+        .returning({ uid: user.id });
+    }
+  }
+  if (uidList && uidList.length > 0) {
+    await createSession(uidList[0].uid, userIdentifier, type);
+  } else {
+    throw new Error("login failed");
+  }
+}
+
+export async function loginFromTest(formData: FormData) {
+  const studentId = formData.get("studentId") as string;
+  console.log("loginFromTest", studentId);
+  const uidList = await db
+    .select({
+      uid: user.id,
+    })
+    .from(user)
+    .where(eq(user.studentId, studentId));
+  if (uidList && uidList.length > 0) {
+    await createSession(uidList[0].uid, studentId, "test");
+  } else {
+    throw new Error("login failed");
+  }
 }
 
 export async function logout() {
-	deleteSession();
-	redirect("/login");
+  deleteSession();
+  redirect("/login");
 }
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,39 +1,42 @@
 import { FeishuSDKInject } from "@/components/feishuInject";
+import { TestLogin } from "@/components/testLogin";
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Separator } from "@/components/ui/separator";
 import { verifySession } from "@/lib/dal";
+import { ArrowRight } from "lucide-react";
 import Image from "next/image";
 import { redirect } from "next/navigation";
+import { useFormStatus } from "react-dom";
 
 const Login = async () => {
-	return (
-		<>
-			<div className="h-screen w-screen grid grid-cols-1 lg:grid-cols-2">
-				<div className="h-full bg-black text-white hidden col-span-1 p-5 lg:flex justify-between flex-col">
-					<div>
-						<Image
-							src="/images/white-logo.png"
-							alt="123123"
-							width={100}
-							height={50}
-						/>
-					</div>
-					<div>"开源平等，薪火相传"</div>
-				</div>
-				<div className="h-full flex justify-center items-center flex-col gap-8 col-span-1">
-					<div className="flex gap-2 flex-col items-center">
-						<div className="text-2xl font-semibold">
-							登录到 SAST Pass
-						</div>
-						<div className="text-sm text-gray-500">
-							开启你的科协之旅
-						</div>
-					</div>
-					<Button>使用 SAST Link 登录</Button>
-				</div>
-			</div>
-			<FeishuSDKInject />
-		</>
-	);
+  return (
+    <>
+      <div className="h-screen w-screen grid grid-cols-1 lg:grid-cols-2">
+        <div className="h-full bg-black text-white hidden col-span-1 p-5 lg:flex justify-between flex-col">
+          <div>
+            <Image
+              src="/images/white-logo.png"
+              alt="123123"
+              width={100}
+              height={50}
+            />
+          </div>
+          <div>"开源平等，薪火相传"</div>
+        </div>
+        <div className="h-full flex justify-center items-center flex-col gap-8 col-span-1">
+          <div className="flex gap-2 flex-col items-center">
+            <div className="text-2xl font-semibold">登录到 SAST Pass</div>
+            <div className="text-sm text-gray-500">开启你的科协之旅</div>
+          </div>
+          <Button>使用 SAST Link 登录</Button>
+          {process.env.NODE_ENV === "development" && <TestLogin />}
+        </div>
+      </div>
+      <FeishuSDKInject />
+    </>
+  );
 };
 
 export default Login;
